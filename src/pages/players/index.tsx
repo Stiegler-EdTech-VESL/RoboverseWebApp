e0094223fd58dd645698e19d40afa5a7fb35df77
Erin Woodard <erin@stiegleredtech.org>
2023-11-01 13:46:45 -0400

    import PlayersList to players index

--- src/pages/players/index.tsx
+++ src/pages/players/index.tsx
@@ -1,3 +1,15 @@
-export default function PlayersPage() {
-  return <div>Players</div>;
+import PlayersList from "../components/PlayersList";
+import { FC } from 'react';
+
+const Players : FC = () => {
+  return (
+    <div className="flex items-center justify-center">
+    <div className="mx-20 mt-4 flex-grow self-center pb-4 text-center  text-3xl text-slate-300">
+      {/* <TeamsList distID={selectedValue} /> */}
+      <PlayersList></PlayersList>
+    </div>
+  </div>
+  )
 }
+
+export default Players;
