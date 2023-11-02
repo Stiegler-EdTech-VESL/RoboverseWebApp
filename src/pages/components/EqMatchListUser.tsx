6d393390a6e9d44c31dc78ef973f5cd78d037194
Erin Woodard <erin@stiegleredtech.org>
2023-11-02 17:44:14 -0400

    query data for EqMatchListUser

--- src/pages/components/EqMatchListUser.tsx
+++ src/pages/components/EqMatchListUser.tsx
@@ -6,12 +6,14 @@ import ProfileNamePic from './ProfileNamePic';
 import { useRouter } from 'next/router';
 import { match } from 'assert';
 import { EquationMatch } from '@prisma/client';
+import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell } from '@nextui-org/react';
 
-interface eqMatchListProps {
+interface eqMatchListUserProps {
     userId: string,
 }
 
-const EqMatchListUser: FC<eqMatchListProps> = ({ userId}) => {
+
+const EqMatchListUser: FC<eqMatchListUserProps> = ({ userId}) => {
     
     const user = api.users.getUserById.useQuery({ id: userId },{enabled: !!userId});
     const userEqMatches = user.data?.UserInEquationMatch;
@@ -23,68 +25,56 @@ const EqMatchListUser: FC<eqMatchListProps> = ({ userId}) => {
 
     const matchObjList = api.eqMatches.getEqMatches.useQuery({ids: matchIdList});
 
-    // if (!user?.data) {
-    //   return (
-    //     <div className=" flex items-center justify-center text-5xl italic">
-    //       <div className="fles flex-col items-center justify-center ">
-    //         <Image
-    //           className="mt-[50%]"
-    //           width={200}
-    //           height={200}
-    //           alt={"Loading"}
-    //           src={"/spinner.svg"}
-    //         ></Image>
-    //       </div>
-    //     </div>
-    //   );
-    // } else {
+    if (!matchObjList?.data) {
       return (
-        <div className="w-full">
-          {/* <table className="w-full text-2xl">
-            <thead className="sticky top-0 bg-green-500 text-black">
-              <tr>
-                <th className="px-4 py-2">Name / Created By</th>
-                <th className="px-4 py-2">Contributed Elo</th>
-                <th className="px-4 py-2">Total Matches</th>
-              </tr>
-            </thead>
-            <tbody>
-              {user.data.UserInEquationMatch.map((eq) => {
-                if (!eq) {
-                  return null;
-                }
-                const totalMatches = user.data.UserInEquationMatch.length;
-                const user = eq.User;
-  
-                return (
-                  <tr key={eq.id}>
-                    <td className="flex items-center justify-center px-4 py-2 text-center ">
-                      <Link
-                        className="hover:underline"
-                        href={"/equations/" + eq.id}
-                      >
-                        {eq.name}
-                      </Link>
-                      {user ? (
-                        <ProfileNamePic
-                          image={user.image ? user.image : null}
-                          name={user.name}
-                          showName={false}
-                        />
-                      ) : (
-                        ""
-                      )}
-                    </td>
-                    <td className="px-4 py-2 text-center">{eq.elo_contribute}</td>
-                    <td className="px-4 py-2 text-center">{totalMatches}</td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table> */}
+        <div className=" flex items-center justify-center text-5xl italic">
+          <div className="fles flex-col items-center justify-center ">
+            <Image
+              className="mt-[50%]"
+              width={200}
+              height={200}
+              alt={"Loading"}
+              src={"/spinner.svg"}
+            ></Image>
+          </div>
         </div>
       );
-    // }
+    } else {
+      return (
+        <Table className=" rounded-md">
+          <TableHeader className="">
+            <TableColumn className="bg-green-500 text-black">Date</TableColumn>
+            <TableColumn className="bg-green-500 py-2 text-black">
+              Match Type
+            </TableColumn>
+            <TableColumn className="bg-green-500 text-black">
+              Score
+            </TableColumn>
+            <TableColumn className="bg-green-500 text-black">
+              Rank After
+            </TableColumn>
+          </TableHeader>
+          <TableBody
+            className=" rounded-md bg-gray-900"
+            emptyContent={"No rows to display."}
+          >
+        {
+            matchObjList.data!.map((match)=>{
+                let i = matchObjList.data?.indexOf(match);
+                return <TableRow key={i}>
+                    <TableCell>
+                        {String(match.ended)}
+                    </TableCell>
+                    <TableCell>
+                        {match.type}
+                    </TableCell>
+                </TableRow>
+            })
+        }
+          </TableBody>
+        </Table>
+      );
+    }
 };
 
 export default EqMatchListUser;
