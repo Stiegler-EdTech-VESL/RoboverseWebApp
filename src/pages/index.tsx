56fb4e95fb0cc60ffdd3d7eb459ab60b99c6c186
Erin Woodard <erin@stiegleredtech.org>
2023-11-06 16:49:09 -0500

    check for User.team_id

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,6 @@
-import { useSession } from "next-auth/react";
-// import Head from "next/head";
-// import Link from "next/link";
-// import { api } from "~/utils/api";
-// import Image from "next/image";
 import HomePage from "./components/HomePage";
-import { useQuery, gql } from "@apollo/client";
-
-const TESTING = gql`
-query {
-  user(where: {discord_id: {_eq: "856213920913686580"}}) {
-    school_id
-  }
-}
-`;
 
 export default function Home() {
-
-  const {data, loading, error} = useQuery(TESTING);
-  if(loading){
-    console.log("pooooping")
-  }
-
-  if(error){
-    console.log(error)
-  }
-
-  if(data){
-
-  console.log("Letttts gooo: " + JSON.stringify(data.user[0].school_id));
-}
-
-  const { data: session } = useSession();
-
-  console.log(JSON.stringify(session));
 
   return (
     <>
--- src/server/auth.ts
+++ src/server/auth.ts
@@ -6,10 +6,14 @@ import {
   type DefaultSession,
 } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
-import OAuthProvider from "next-auth/providers/oauth";
+import { DiscordProfile } from "next-auth/providers/discord";
 import { type ReactNode } from "react";
 import { env } from "~/env.mjs";
 import { prisma } from "~/server/db";
+import { gql } from "@apollo/client";
+import { client } from "../ApolloClient/client";
+
+
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
@@ -25,10 +29,6 @@ declare module "next-auth" {
     };
   }
 
-  // interface User {
-  //   // ...other properties
-  //   // role: UserRole;
-  // }
 }
 
 /**
@@ -36,19 +36,71 @@ declare module "next-auth" {
  *
  * @see https://next-auth.js.org/configuration/options
  */
+
+
 export const authOptions: NextAuthOptions = {
   callbacks: {
-    session: ({ session, user }) => ({
-      ...session,
-      user: {
-        ...session.user,
-        id: user.id,
-        name: user.name,
-        image: user.image,
-        email: user.email,
-      },
-    }),
-    
+    session: async ({ session, user }) => {
+      return Promise.resolve({
+        ...session,
+        user: {
+          ...session.user,
+          id: user.id,
+          name: user.name,
+          image: user.image,
+          email: user.email,
+        },
+      });
+    },
+
+    async signIn({ user, account, profile }) {
+      // GraphQL query for the Discord Bot DB
+      const GET_USER_QUERY = gql`
+        query GetUser($discordId: String!) {
+          user(where: { discord_id: { _eq: $discordId } }) {
+            school_id
+          }
+        }
+      `;
+
+      try {
+        // Execute the query with the user's Discord ID
+        const { data } = await client.query({
+          query: GET_USER_QUERY,
+          variables: {
+            discordId: (profile as DiscordProfile).id,
+          },
+        });
+
+        //If data is returned, find the user in the Roboverse DB
+        if (data && data.user.length > 0) {
+          const currentUser = await prisma.user.findUnique({
+            where: {
+              id: user.id,
+            }
+          })
+          const currentTeam = currentUser?.team_id; //Team ID in the Roboverse DB
+          const realTeam = data.user[0].school_id //School ID in Discord Bot DB
+
+          if(currentTeam !== realTeam) { //If Team ID and School ID are not equal, update the Team ID in Roboverse DB
+            await prisma.user.update({
+              where: {
+                id: user.id,
+              },
+              data: {
+                team_id: realTeam,
+              }
+            });
+          };
+        };
+
+        // Continue with the sign-in process
+        return true;
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+        return false;
+      }
+    },
   },
   adapter: PrismaAdapter(prisma),
   providers: [
@@ -56,37 +108,6 @@ export const authOptions: NextAuthOptions = {
       clientId: env.DISCORD_CLIENT_ID,
       clientSecret: env.DISCORD_CLIENT_SECRET,
     }),
-    // {
-    //   id: "vesl",
-    //   name: "Vesl",
-    //   type: "oauth",
-    //   authorization: "https://app.vesl.gg/version-test/api/1.1/oauth/authorize",
-    //   token: "https://app.vesl.gg/version-test/api/1.1/oauth/access_token",
-    //   userinfo: "https://app.vesl.gg/version-test/api/1.1/obj/user",
-    //   // "https://app.vesl.gg/version-test/api/1.1/obj/user?constraints=[{'key':'Username','onstraint_type':'equals','value':'StarLord'}]",
-    //   clientId: env.VESL_CLIENT_ID,
-    //   clientSecret: env.VESL_CLIENT_SECRET,
-
-    //   profile: (profile) => {
-    //     console.log("profile", profile);
-    //     return {
-    //       id: profile.id,
-    //       name: profile.name,
-    //       email: profile.email,
-    //       image: profile.image,
-    //     };
-    //   },
-    // },
-
-    /**
-     * ...add more providers here.
-     *
-     * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
-     * @see https://next-auth.js.org/providers/github
-     */
   ],
 };
 
