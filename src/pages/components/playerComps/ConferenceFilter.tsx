8fcba26de4bc2d2e14c70fe54a44296534c32a8f
Erin Woodard <erin@stiegleredtech.org>
2023-11-13 10:59:18 -0500

    styles for ConferenceFilter

--- src/pages/components/playerComps/ConferenceFilter.tsx
+++ src/pages/components/playerComps/ConferenceFilter.tsx
@@ -62,9 +62,10 @@ const ConferenceFilter: FC<Props> = ({ conference, onConferenceChange }) => {
   ];
 
   return (
+    <div className="my-5">
     <Dropdown className="rounded-sm border bg-black">
       <DropdownTrigger>
-        <Button variant="bordered" className="rounded-sm border px-4 py-2 w-[200px] text-sm">
+        <Button variant="bordered" className="rounded-sm border px-4 py-2 w-[200px] text-sm hover:border-green-500 hover:text-green-500">
           {conference}
         </Button>
       </DropdownTrigger>
@@ -75,10 +76,11 @@ const ConferenceFilter: FC<Props> = ({ conference, onConferenceChange }) => {
         onAction={handleFilterChange}
       >
         {items.map((item) => {
-          return <DropdownItem className="cursor-pointer" key={item.key}>{item.label}</DropdownItem>;
+          return <DropdownItem className="cursor-pointer hover:text-green-500" key={item.key}>{item.label}</DropdownItem>;
         })}
       </DropdownMenu>
     </Dropdown>
+    </div>
   );
 };
 
