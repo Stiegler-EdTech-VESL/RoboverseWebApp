bc8e61e36607b3a83ace4f924feec0bb11c512f0
Erin Woodard <erin@stiegleredtech.org>
2023-11-14 14:27:11 -0500

    player page styles

--- src/pages/players/[player].tsx
+++ src/pages/players/[player].tsx
@@ -23,21 +23,19 @@ const Player = () => {
   }
 
   const player = api.users.getUserByName.useQuery({ name: playerName });
-  const rankTitle = images.titles[player.data?.global_rank_title as keyof typeof images.titles];
-
-
+  const rankTitle =
+    images.titles[player.data?.global_rank_title as keyof typeof images.titles];
 
   return (
     <>
-      <div className="flex flex-col gap-2 flex-wrap items-center place-content-around py-10 mx-20">
-        
-      <h1 className="text-4xl font-semibold text-white">
-              {player.data?.name}
-            </h1>
-        <div className="flex flex-row items-center place-content-around rounded-md bg-white bg-opacity-20 p-5 mb-10">
-          <div className="shrink w-1/3">
+      <div className="mx-20 flex flex-col flex-wrap place-content-around items-center gap-2 py-10">
+        <h1 className="text-4xl font-semibold text-white">
+          {player.data?.name}
+        </h1>
+        <div className="mb-10 flex flex-row place-content-around items-center rounded-md bg-white bg-opacity-20 p-5">
+          <div className="w-1/3 shrink">
             <Image
-              className="object-contain mx-auto"
+              className="mx-auto object-contain"
               alt={player.data?.name + "'s Profile Picture"}
               src={player.data?.image ?? "/spinner.svg"}
               width={200}
@@ -47,36 +45,35 @@ const Player = () => {
           </div>
           {/* team name and stuff */}
           <div className="w-1/3">
-          <div className="flex flex-col gap-2">
-            <div className="flex flex-row gap-2 text-lg w-fit">
-              <h2 className="font-md italic text-slate-500">
-                Ranking:
-              </h2>
-              <p className="slate-200">
-                {(Number(player.data?.global_ranking)*1000).toFixed(0)}
-              </p>
-            </div>
-            <div className="flex flex-row gap-2 text-lg w-fit">
-              <h2 className="font-medium italic text-slate-500">W/L:</h2>
-              <p>
-                {player.data?.totalEqMatchesWon} / {player.data?.totalEqMatchesLost}
-              </p>
-            </div>
-           <div className="w-1/3">
-            <PlayerHistory id={player.data?.id!} />
-            </div>
+            <div className="flex flex-col gap-3">
+
+              <div className="flex w-fit flex-row gap-2 text-lg">
+                <h2 className="font-md italic text-slate-500">W/L:</h2>
+                <p className="font-semibold text-white">
+                  {player.data?.totalEqMatchesWon} /{" "}
+                  {player.data?.totalEqMatchesLost}
+                </p>
+              </div>
+          
+                <h2 className="font-md italic text-slate-500 text-lg">Rating:</h2>
+              
+              <PlayerHistory id={player.data?.id!} />
             </div>
           </div>
-          <div className="w-1/3 flex flex-col gap-2 items-center text-xl">
-            {rankTitle}
-          <RankImage team_rank_title={player.data?.global_rank_title || "Charcoal"} width={100} height={100} />
+          <div className="flex w-1/3 flex-col items-center text-xl">
+            <div className="flex h-[200px] w-[200px] flex-col items-center justify-center gap-2 rounded-md bg-zinc-800">
+              {rankTitle}
+              <RankImage
+                team_rank_title={player.data?.global_rank_title || "Charcoal"}
+                width={100}
+                height={100}
+              />
+            </div>
           </div>
         </div>
-        
-        
+
         <EqMatchListUser userId={player.data?.id!}></EqMatchListUser>
       </div>
-
     </>
   );
 };
