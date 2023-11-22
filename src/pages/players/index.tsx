22ee95223ac63ee28d3bc0d24a66c0bbfb5b955e
erin-stiegleredtech <erin@stiegleredtech.org>
2023-11-22 14:59:12 -0500

    modal for ranks

--- src/pages/players/index.tsx
+++ src/pages/players/index.tsx
@@ -2,22 +2,32 @@ import PlayersList from "../../components/playerComps/PlayersList";
 import { FC, useState } from "react";
 import TopPlayers from "../../components/playerComps/TopPlayers";
 import ConferenceFilter from "../../components/playerComps/ConferenceFilter";
+import RankGuide from "~/components/RankGuide";
 
 const Players: FC = () => {
   const [filterState, setFilterState] = useState("All Conferences");
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
-    <div className="flex items-center justify-center">
-      <div className="mx-20 mt-4 flex-grow self-center pb-4 text-center text-slate-300">
-
-        <TopPlayers />
-        <ConferenceFilter
-          conference={filterState}
-          onConferenceChange={setFilterState}
-        />
-        <PlayersList conference={filterState}></PlayersList>
+    <>
+      <RankGuide
+        isModalOpen={isModalOpen}
+        onModalChange={setIsModalOpen}
+      ></RankGuide>
+      <div className="flex items-center justify-center">
+        <div className="mx-20 mt-4 flex-grow self-center pb-4 text-center text-slate-300">
+          <TopPlayers />
+          <ConferenceFilter
+            conference={filterState}
+            onConferenceChange={setFilterState}
+          />
+          <PlayersList
+            conference={filterState}
+            onModalChange={setIsModalOpen}
+          ></PlayersList>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
